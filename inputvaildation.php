b21b028ef587ef9478a0070e844647f5a7226ca3
Pallavi6295 <pallavi05madhu@gmail.com>
2025-12-24 12:31:18 +0530

    Add files via upload

---
+++ inputvaildation.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>Input Validation</title>
+</head>
+<body>
+<form method = "post">
+name: <input type="text" name="name"><br><br>
+email: <input type="text" name="email"><br><br>
+<input type="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
+
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+$name=$_POST['name'];
+$email=$_POST['email'];
+
+if(empty($name)){
+echo "Name is required.<br><br>";
+} else{
+echo "Name:".$name."<br><br>";
+}
+
+if(empty($email)){
+echo "Email is required.<br><br>";
+}elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+echo "Invalid email format.<br><br>";
+}else{
+echo "Email:".$email."<br><br>";
+}
+}
+?>
